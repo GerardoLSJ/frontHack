3e24eb9463e114609f58b8ba3debffb7262ed31e
Sebastián Silva <sebastian@booxlab.com>
2018-09-08 13:12:46 -0500

    List categories

--- src/pages/new-post/new-post.ts
+++ src/pages/new-post/new-post.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import {Validators, FormBuilder, FormGroup } from '@angular/forms';
+import { HttpClient } from '@angular/common/http'
 
 /**
  * Generated class for the NewPostPage page.
@@ -16,31 +17,54 @@ import {Validators, FormBuilder, FormGroup } from '@angular/forms';
 })
 export class NewPostPage {
 
-  private post : FormGroup;
+  private post : FormGroup
+  public tagList: any
 
   constructor(
     public navCtrl: NavController, 
     public navParams: NavParams,  
-    private formBuilder: FormBuilder){
+    private formBuilder: FormBuilder, 
+    public http: HttpClient ){
     
+    this.getTags()
+
     this.post = this.formBuilder.group({
       title: ['', Validators.required],
       summary: [''],
       bullets: [], 
       law: '', 
       reference: '', 
-      tags: [], 
-      notes: ''
+      tags: [''], 
+      notes: 'Sin comentarios'
       });
     }
 
 
   logForm(){
     console.log(this.post.value)
-  }
+    this.http.post('https://lex-app48.herokuapp.com/api/law/', this.post.value)
+      .subscribe(res => {
+        console.log(res)
+        console.log('éxito')
+        this.navCtrl.pop()
+      }, (err) => {
+        console.log(err);
+      });
+    
+    }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad NewPostPage');
+  }
+
+
+  getTags(){
+    this.http
+    .get('https://lex-app48.herokuapp.com/api/tags/?format=json')
+    .subscribe((data: any) => {
+      console.log(data)
+      this.tagList = data
+    })
   }
 
 }
